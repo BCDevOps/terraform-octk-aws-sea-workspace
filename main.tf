2e63fd0fff384a194df814161a5d0d8963d59e3c
Warren Uniewski <warren@autimo.com>
2021-12-06 11:32:27 -0800

    bump project_wprkspace to v0.0.3

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 
 provider "aws" {
-	version = "~> 3.0"
-	region = "ca-central-1"
+  version = "~> 3.0"
+  region  = "ca-central-1"
 
-	assume_role {
-		role_arn = "arn:aws:iam::${var.master_account_id}:role/${var.automation_role_name}"
-		session_name = "slz-terraform-automation"
-	}
+  assume_role {
+    role_arn     = "arn:aws:iam::${var.master_account_id}:role/${var.automation_role_name}"
+    session_name = "slz-terraform-automation"
+  }
 }
 
 
 locals {
-	project = jsondecode(file(var.project_definition_file_path))
+  project = jsondecode(file(var.project_definition_file_path))
 }
 
 module "project_workspace" {
-	source = "github.com/BCDevOps/terraform-aws-sea-account-set.git?ref=v0.0.2"
-	project = local.project
-	org_admin_role_name = var.org_admin_role_name
-	account_email_prefix = var.account_email_prefix
-	account_email_domain = var.account_email_domain
+  source               = "github.com/BCDevOps/terraform-aws-sea-account-set.git?ref=v0.0.3"
+  project              = local.project
+  org_admin_role_name  = var.org_admin_role_name
+  account_email_prefix = var.account_email_prefix
+  account_email_domain = var.account_email_domain
 }
 
