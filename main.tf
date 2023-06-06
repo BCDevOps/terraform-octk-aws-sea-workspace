134ac67c4f25afadae26773e351c38ce4cb0639b
Prabhu-Manchineella_bcgovent <prabhu.manchineella@gov.bc.ca>
2023-06-06 13:06:57 -0400

    bump tag to v0.0.8 which has support automation

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "project_workspace" {
-  source               = "github.com/BCDevOps/terraform-aws-sea-account-set.git?ref=v0.0.7"
+  source               = "github.com/BCDevOps/terraform-aws-sea-account-set.git?ref=v0.0.8"
   project              = local.project
   org_admin_role_name  = var.org_admin_role_name
   account_email_prefix = var.account_email_prefix
